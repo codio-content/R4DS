14797838797d3806cdb99fa8e252eb2a8e29720b
Elise Deitrick <edeitrick@codio.com>
2019-06-27 01:00:38 +0000

    layout thru strings

---
+++ code/otherPatterns.r
@@ -0,0 +1,10 @@
+# write your code below and then press the RUN CODE button
+# for graphical output press the "Refresh Plot" link
+ 
+library(tidyverse)
+library(stringr)
+
+# The regular call:
+str_view(fruit, "nana")
+# Is shorthand for
+str_view(fruit, regex("nana"))
